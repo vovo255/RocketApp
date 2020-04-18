dee82c4ade59f8f363a7cf46d0936e6751e52a8a
vovo255 <33264648+vovo255@users.noreply.github.com>
2020-04-18 15:39:43 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+
+class Container(BoxLayout):
+    pass
+
+class MyApp(App):
+    def build(self):
+        return Container()
+
+
+if __name__ == '__main__':
+    MyApp().run()
